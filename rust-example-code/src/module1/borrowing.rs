0b1d0b8c3c6cfca6ca3027c3e3f9f30caa8538af
tediace <acetedi7@gmail.com>
2024-12-18 11:01:14 +0700

    add some explanation code for slice type

--- rust-example-code/src/module1/borrowing.rs
+++ rust-example-code/src/module1/borrowing.rs
@@ -10,9 +10,17 @@ pub fn references_borrowing() {
 }
 
 pub fn first_word(s: &String) -> usize {
+    // as_bytes is changes string into slice array
+    // hello_world (string), 012345678910 (slice array)
     let bytes = s.as_bytes();
 
+    // enumerate added index (i) each element of iteration
+    // iter given reference (&u8) in slice byte
+    // because iter give reference of value string (hello world)
+    // we need to dereference them using & (&item)
+    // or manually (item), let item = *item
     for (i, &item) in bytes.iter().enumerate() {
+        // b ' ' is byte literal in Rust and represent of character space
         if item == b' ' {
             return i; // if condition met get value from return i
             // 5
